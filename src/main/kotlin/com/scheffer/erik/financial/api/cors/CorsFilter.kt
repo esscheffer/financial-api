5e3e8ccb0c8bf4e7ff7ecb19b2e8dd4f58722717
Erik de Souza Scheffer <erikscheffer@yahoo.com>
2019-10-16 17:57:16 -0300

    Change cors configuration to a filter

---
+++ src/main/kotlin/com/scheffer/erik/financial/api/cors/CorsFilter.kt
@@ -0,0 +1,33 @@
+package com.scheffer.erik.financial.api.cors
+
+import org.springframework.core.Ordered
+import org.springframework.core.annotation.Order
+import org.springframework.stereotype.Component
+import javax.servlet.Filter
+import javax.servlet.FilterChain
+import javax.servlet.ServletRequest
+import javax.servlet.ServletResponse
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+class CorsFilter : Filter {
+    override fun doFilter(req: ServletRequest?, resp: ServletResponse?, chain: FilterChain?) {
+        val request = req as HttpServletRequest
+        val response = resp as HttpServletResponse
+
+        response.setHeader("Access-Control-Allow-Origin", "*")
+        response.setHeader("Access-Control-Allow-Credentials", "true")
+
+        if (request.method == "OPTIONS") {
+            response.setHeader("Access-Control-Allow-Methods", "POST, GET, DELETE, PUT, OPTIONS")
+            response.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type, Accept")
+            response.setHeader("Access-Control-Max-Age", "3600")
+
+            response.status = HttpServletResponse.SC_OK
+        } else {
+            chain?.doFilter(req, resp)
+        }
+    }
+}
